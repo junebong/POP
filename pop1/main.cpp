9d08f8a15e1905cef6c3e4234d659b3a47d08ccf
junebong <junebongkim@gmail.com>
2014-03-03 21:45:23 +0900

    pop1新規作成

---
+++ pop1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  pop1
+//
+//  Created by 金 俊峰 on 2014/03/03.
+//
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
